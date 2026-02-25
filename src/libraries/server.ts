e0bed00f1096d963a93ff6a73d0bc43ac2ddbe65
streamamos <134351107+streamamos@users.noreply.github.com>
2026-02-25 13:04:25 +0000

    Add CORS headers to server response
    Added CORS headers to the response for both HTTPS and HTTP requests.

--- src/libraries/server.ts
+++ src/libraries/server.ts
@@ -668,6 +668,10 @@ export async function proxyTs(url: string, headers: any, req, res: http.ServerRe
         if (forceHTTPS) {
             const proxy = https.request(options, (r) => {
                 r.headers["content-type"] = "video/mp2t";
+                r.headers["access-control-allow-origin"] = "*";
+                r.headers["access-control-allow-methods"] = "*";  
+                r.headers["access-control-allow-headers"] = "*";
+                
                 res.writeHead(r.statusCode ?? 200, r.headers);
 
                 r.pipe(res, {
@@ -681,6 +685,10 @@ export async function proxyTs(url: string, headers: any, req, res: http.ServerRe
         } else {
             const proxy = http.request(options, (r) => {
                 r.headers["content-type"] = "video/mp2t";
+                r.headers["access-control-allow-origin"] = "*";
+                r.headers["access-control-allow-methods"] = "*";  
+                r.headers["access-control-allow-headers"] = "*";
+                
                 res.writeHead(r.statusCode ?? 200, r.headers);
 
                 r.pipe(res, {
